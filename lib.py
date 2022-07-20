966a60055f66f5221cf3c46aa3a194d7946dbeb0
rohihtvp <rohithpandian13@gmail.com>
2022-07-20 16:30:46 +0530

    Simple Book

--- lib.py
+++ lib.py
@@ -1,16 +1,49 @@
-from flask import Flask, render_template
+from crypt import methods
+from pickle import TRUE
+from flask import Flask, redirect, render_template, request, url_for
+import pandas as pd
+
+import mysql.connector
 app = Flask(__name__)
 
+connection = mysql.connector.connect(host='localhost',
+                                     database='Books',
+                                     user='root',
+                                     password='mysql123')
 
-@app.route("/")
+#cur.execute("SELECT * FROM Book")
+
+
+@app.route('/', methods=['POST', 'GET'])
 def home():
     return render_template("index.HTML")
 
 
-@app.route("/Book")
+@app.route('/Book', methods=['POST', 'GET'])
 def Book():
-    return render_template("desc.HTML")
+
+    if request.method == 'POST':
+        #Data = request.form.get
+        id = request.form['id']
+        Book_name = request.form['Book_name']
+        Author_name = request.form['Author_name']
+        Stock = request.form['Stock']
+        cur = connection.cursor()
+        cur.execute(''' INSERT INTO Book VALUES(%s,%s,%s,%s)''',
+                    (id, Book_name, Author_name, Stock))
+
+        connection.commit()
+        cur.close()
+        return f'Book Added'
+
+
+@app.route('/list', methods=['POST', 'GET'])
+def list():
+    cur = connection.cursor()
+    cur.execute('''SELECT * FROM Book''')
+    data = cur.fetchall()
+    return render_template('data.HTML', data=data)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=TRUE)
